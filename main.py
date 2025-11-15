ed819176f7b6dfb3e60a636130aa00f019d0589a
elenachau <elenaxcha@gmail.com>
2025-11-14 16:06:37 -0800

    get and open database

--- main.py
+++ main.py
@@ -2,31 +2,21 @@ from dataclasses import dataclass
 from datetime import date
 from fastapi import FastAPI
 
+from hotel.db.engine import DBSession, init_db
+from hotel.db.models import DBRoom
+from hotel.routers import rooms
+
 app = FastAPI()
+
+# initialize db
+DB_FILE = "sqlite:///hotel.db"
+
+@app.on_event("startup")
+def startup_event(): # open db
+    init_db(DB_FILE)
 
 @app.get("/")
 def read_root():
     return "The server is running"
 
-@dataclass
-class Customer:
-    id: int
-    first_name: str
-    last_name: str
-    email_address: str
-
-@dataclass
-class Room:
-    id: int
-    number: str
-    size: int
-    price: int
-
-@dataclass
-class Booking:
-    id: int
-    from_date: date
-    to_date: date
-    customer: Customer
-    room: Room
-    price: int
+app.include_router(rooms.router)
