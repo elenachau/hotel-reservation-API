a7f024249bca332a6e4123e8f87030bd12898e91
elenachau <elenaxcha@gmail.com>
2025-11-16 12:00:31 -0800

    update booking router with interface

--- hotel/routers/bookings.py
+++ hotel/routers/bookings.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
+from hotel.db.db_interface import DBInterface
+from hotel.db.models import DBBooking, DBRoom
 from hotel.operations.bookings import (
     BookingCreateData, 
-    BookingUpdateData, 
     create_booking, 
     delete_booking, 
     read_all_bookings, 
     read_booking, 
-    delete_booking
 )
 
 # groups of request the server will handle
@@ -15,18 +15,22 @@ router = APIRouter()
 
 @router.get("/bookings")
 def api_read_all_bookings():
-    return read_all_bookings()
+    booking_interface = DBInterface(DBBooking) # create interface
+    return read_all_bookings(booking_interface)
 
 
 @router.get("/booking/{booking_id}")
 def api_read_booking(booking_id:int):
-    return read_booking(booking_id)
+    booking_interface = DBInterface(DBBooking)
+    return read_booking(booking_id, booking_interface)
 
 
 @router.post("/booking")
-def api_create_booking(booking: BookingCreateData)
-    return create_booking(booking)
-
+def api_create_booking(booking: BookingCreateData):
+    booking_interface = DBInterface(DBBooking)
+    room_interface = DBInterface(DBRoom)
+    return create_booking(booking, booking_interface, room_interface)
+Ç
 
 @router.delete("/booking/{booking_id}")
 def api_delete_booking(booking_id: int):
