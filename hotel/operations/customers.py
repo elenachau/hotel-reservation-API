e629413e2f99f7580ccd12728bcda6f2095019bc
elenachau <elenaxcha@gmail.com>
2025-11-15 16:40:36 -0800

    convert customer obj to raw data

---
+++ hotel/operations/customers.py
@@ -0,0 +1,43 @@
+from typing import Optional
+from hotel.db.engine import DBSession
+from hotel.db.models import DBCustomer, to_dict
+from pydantic import BaseModel
+
+class CustomerCreateData(BaseModel):
+    first_name: str
+    last_name: str
+    email_address: str
+
+
+class CustomerUpdateData(BaseModel):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    email_address: Optional[str]
+
+
+def read_all_customers():
+    session = DBSession()
+    customers: list[DBCustomer] = session.query(DBCustomer).all()
+    return [to_dict(c) for c in customers]
+
+
+def read_customer(customer_id: int):
+    session = DBSession()
+    customer = session.query(DBCustomer).get(customer_id)
+    return to_dict(customer)
+
+
+def create_customer(data: CustomerCreateData):
+    session = DBSession()
+    customer = DBCustomer(**data.dict()) # create customer object, convert data to dict
+    session.add(customer)
+    session.commit()
+    return to_dict(customer) # convert to raw data for API to return
+
+def update_customer(customer_id: int, data: CustomerUpdateData):
+    session = DBSession()
+    customer = session.query(DBCustomer).get(customer_id)
+    for key, value in data.dict(exclude_none=True).items():
+        setattr(customer, key, value)
+    session.commit()
+    return to_dict(customer)
