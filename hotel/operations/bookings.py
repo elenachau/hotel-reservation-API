01bf0f457083105eface41bb1149531de88ffb7c
elenachau <elenaxcha@gmail.com>
2025-11-16 11:55:21 -0800

    abstract booking methods with interface

--- hotel/operations/bookings.py
+++ hotel/operations/bookings.py
@@ -1,12 +1,12 @@
 from datetime import date
-from typing import Optional
-from hotel.db.engine import DBSession
-from hotel.db.models import DBBooking, DBRoom, to_dict
 from pydantic import BaseModel
+from hotel.operations.interface import DataInterface, DataObject
+
 
 # create custom exception
 class InvalidDateError(Exception):
     pass
+
 
 class BookingCreateData(BaseModel):
     room_id: int
@@ -22,39 +22,32 @@ class BookingUpdateData(BaseModel):
     to_date: date
 
 
-def read_all_bookings():
-    session = DBSession()
-    bookings: list[DBBooking] = session.query(DBBooking).all()
-    return [to_dict(b) for b in bookings]
+def read_all_bookings(booking_interface: DataInterface) -> list[DataObject]:
+    return booking_interface.read_all()
 
 
-def read_booking(booking_id: int):
-    session = DBSession()
-    booking = session.query(DBBooking).get(booking_id)
-    return to_dict(booking)
+def read_booking(booking_id: int, booking_interface: DataInterface) -> DataObject:
+    return booking_interface.read_by_id(booking_id)
 
 
-def create_booking(data: BookingCreateData):
-    session = DBSession()
+def create_booking(
+    data: BookingCreateData,
+    booking_interface: DataInterface,
+    room_interface: DataInterface,
+) -> DataObject:
 
     # retrieve the room
-    room = session.query(DBRoom).get(data.room_id)
+    room = room_interface.read_by_id(data.room_id)
 
     days = (data.to_date - data.from_date).days
     if days <= 0:
         raise InvalidDateError("Invalid dates.")
-    
+
     booking_dict = data.dict()
-    booking_dict["price"] = room.price * days
+    booking_dict["price"] = room["price"] * days  # dealing with dict now
 
-    booking = DBBooking(**data.dict()) # create booking object, convert data to dict
-    session.add(booking)
-    session.commit()
-    return to_dict(booking) # convert to raw data for API to return
+    return booking_interface.create(booking_dict)
 
-def delete_booking(booking_id: int):
-    session = DBSession()
-    booking = session.query(DBBooking).get(booking_id) # retrieve booking
-    session.delete(booking)
-    session.commit()
-    return to_dict(booking)
+
+def delete_booking(booking_id: int, booking_interface: DataInterface) -> DataObject:
+    return booking_interface.delete(booking_id)
