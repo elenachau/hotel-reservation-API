1328148e74d59927d5f5166c71ea3c1b3e11d19f
elenachau <elenaxcha@gmail.com>
2025-11-15 16:39:11 -0800

    create customer requests

---
+++ hotel/routers/customers.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from hotel.operations.customers import CustomerCreateData, CustomerUpdateData, create_customer, read_all_customers, read_customer
+
+# groups of request the server will handle
+router = APIRouter()
+
+
+@router.get("/customers")
+def api_read_all_customers():
+    return read_all_customers()
+
+
+@router.get("/customer/{customer_id}")
+def api_read_customer(customer_id:int):
+    return read_customer(customer_id)
+
+
+@router.post("/customer")
+def api_create_customer(customer: CustomerCreateData)
+    return create_customer(customer)
+
+
+@router.post("/customer/{customer_id}")
+def api_update_customer(customer_id: int, customer: CustomerUpdateData):
+    return update_customer(customer_id, customer)
