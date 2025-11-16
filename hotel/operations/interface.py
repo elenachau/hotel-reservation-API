6e7b18c117d7838915041ce44378d0e7c441c474
elenachau <elenaxcha@gmail.com>
2025-11-15 16:52:21 -0800

    implement abstract data interface

---
+++ hotel/operations/interface.py
@@ -0,0 +1,17 @@
+DataObject = dict[str, Any]
+
+class DataInterface(Protocol):
+    def read_by_id(self, id: int): # read obj fromn id
+        ...
+    
+    def  read_all(self) -> list[DataObject]:
+        ...
+    
+    def create(self, data: DataObject) -> DataObject:
+        ...
+    
+    def update(self, id: int, data: DataObject) -> DataObject:
+        ...
+    
+    def delete(self, id: int) -> DataObject:
+        ...
