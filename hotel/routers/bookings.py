a228592c58c412c2e1d9188a85547cece92b67fe
elenachau <elenaxcha@gmail.com>
2025-11-16 12:36:56 -0800

    adjust spacing

--- hotel/routers/bookings.py
+++ hotel/routers/bookings.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter
 from hotel.db.db_interface import DBInterface
 from hotel.db.models import DBBooking, DBRoom
 from hotel.operations.bookings import (
-    BookingCreateData, 
-    create_booking, 
-    delete_booking, 
-    read_all_bookings, 
-    read_booking, 
+    BookingCreateData,
+    create_booking,
+    delete_booking,
+    read_all_bookings,
+    read_booking,
 )
 
 # groups of request the server will handle
@@ -15,12 +15,12 @@ router = APIRouter()
 
 @router.get("/bookings")
 def api_read_all_bookings():
-    booking_interface = DBInterface(DBBooking) # create interface
+    booking_interface = DBInterface(DBBooking)  # injecting interface
     return read_all_bookings(booking_interface)
 
 
 @router.get("/booking/{booking_id}")
-def api_read_booking(booking_id:int):
+def api_read_booking(booking_id: int):
     booking_interface = DBInterface(DBBooking)
     return read_booking(booking_id, booking_interface)
 
@@ -30,7 +30,7 @@ def api_create_booking(booking: BookingCreateData):
     booking_interface = DBInterface(DBBooking)
     room_interface = DBInterface(DBRoom)
     return create_booking(booking, booking_interface, room_interface)
-Ç
+
 
 @router.delete("/booking/{booking_id}")
 def api_delete_booking(booking_id: int):
